ae82c05fa81061ad11293ae3a964c95b0c24cbb1
Sachid Deshmukh <sadesh@microsoft.com>
2019-10-20 17:10:51 -0700

    Server

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
+server = app.server
 
 soql_url = ('https://data.cityofnewyork.us/resource/nwxe-4ae8.json?' +\
         '$select=boroname,count(tree_id)' +\
